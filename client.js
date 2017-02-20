c1c7204ba7f208eb010e27a3fda7d3e9bf8fae95
Thomas Johander <lopan@Thomas-Johanders-MacBook-Pro.local>
2017-02-20 14:02:16 -0600

    beautified

--- client.js
+++ client.js
@@ -5,41 +5,40 @@ var blueClicks = 0;
 var clickedColor = '';
 
 
-$(document).ready(function(){
-  console.log('document is loaded');
+$(document).ready(function() {
+    console.log('document is loaded');
 
 
-  //button click listeners
-  $('.color-button').on('click',function(){
-    clickedColor = $(this).data('color')
-    incrementClickedColor();
-    drawColorCubeOnDOM();
-  })
-
+    //button click event handler
+    $('.color-button').on('click', function() {
+        clickedColor = $(this).data('color');
+        incrementClickedColor();
+        drawColorCubeOnDOM();
+    })
 })
 
 
 //checks to see the value of clickedColor and increments the appropriate colorClick variable.
-function incrementClickedColor(){
-  if(clickedColor === 'red'){
-    redClicks++;
-    $('#red').text(redClicks);
-  } else if(clickedColor === 'yellow'){
-      yellowClicks++;
-      $('#yellow').text(yellowClicks);
-  } else if(clickedColor === 'green'){
-      greenClicks++;
-      $('#green').text(greenClicks);
-    } else if(clickedColor === 'blue'){
-      blueClicks++;
-      $('#blue').text(blueClicks);
+function incrementClickedColor() {
+    if (clickedColor === 'red') {
+        redClicks++;
+        $('#red').text(redClicks);
+    } else if (clickedColor === 'yellow') {
+        yellowClicks++;
+        $('#yellow').text(yellowClicks);
+    } else if (clickedColor === 'green') {
+        greenClicks++;
+        $('#green').text(greenClicks);
+    } else if (clickedColor === 'blue') {
+        blueClicks++;
+        $('#blue').text(blueClicks);
     }
 }
 
 //appends a new div to the DOM and adds the appropriate class.  CSS handles the background-color property.
-function drawColorCubeOnDOM(){
-  $('.container').append('<div class="color-cube"></div>');
-  $('.color-cube').last().addClass(clickedColor);
+function drawColorCubeOnDOM() {
+    $('.container').append('<div class="color-cube"></div>');
+    $('.color-cube').last().addClass(clickedColor);
 }
 
 
